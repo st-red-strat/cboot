aafd77bc93661fb155cd1957521d533911cc4dc6
Shin T <shintaro.toita@ipmu.jp>
2019-05-08 23:17:03 +0900

    debug3

--- examples/3dBosonic.py
+++ examples/3dBosonic.py
@@ -42,16 +42,11 @@ def bs(delta,upper=3,lower=1,sdp_method=make_SDP):
         prob=sdp_method(delta,{0:D_try})
         prob.write("3d_Ising_binary.xml")
         sdpbargs=[sdpb,"-s","3d_Ising_binary.xml"]+sdpbparams
-        out,err=Popen(sdpbargs,stdout=PIPE,stderr=None,shell=True).communicate()
-#        print(type(out))
-        print(out)
-        sol="dual"
-#        sol=re.compile(r'found ([^ ]+) feasible').search(out).group()[0]
-        if sol==None:
-            raise RuntimeError("Unexpected return from sdpb")
-        elif sol=="dual":
-#            print("(Delta_phi, Delta_epsilon)={0} is excluded."\
-#            .format((float(delta),float(D_try))))
+        out, err=Popen(sdpbargs,stdout=PIPE,stderr=PIPE).communicate()
+        sol=re.compile(r'found ([^ ]+) feasible').search(out).groups()[0] 
+        if sol=="dual":
+            print("(Delta_phi, Delta_epsilon)={0} is excluded."\
+            .format((float(delta),float(D_try)))) 
             upper=D_try
         elif sol=="primal":
             print("(Delta_phi, Delta_epsilon)={0} is not excluded."\
